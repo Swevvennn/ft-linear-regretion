da96e6765a81e9dcf443404d9c51fcb4777ca906
swevvenn <swevvenn@proton.me>
2026-09-23 16:36:29 +0200

    starting project

---
+++ predict/src/main.rs
@@ -0,0 +1,16 @@
+fn reader() {}
+fn parser() {}
+fn normalize() {}
+
+fn epoch() {
+	// calculate predictions
+	// calculate errors
+	// calculate thetas
+	// update thetas
+}
+
+fn writer() {}
+
+fn main() {
+    println!("Hello, world!");
+}
